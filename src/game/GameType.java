6ee76d6f8cadabbbbac582886a15359746e84bcd
mohamedmbh <mohamedbenhamzaa@gmail.com>
2021-12-09 14:12:45 +0100

    Primer commit

---
+++ src/game/GameType.java
@@ -0,0 +1,18 @@
+package game;
+
+public enum GameType {
+
+	SIETE_Y_MEDIA("Siete y Media"),
+	TUTE("Tute"),
+	MUS("Mus");
+
+	private String nombre;
+	
+	GameType(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+}
